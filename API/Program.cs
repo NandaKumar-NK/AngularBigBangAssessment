cb8f05ed781d303fc93801ecea026ab41488cf87
NandaKumar-NK <nanda.kanini@gmail.com>
2023-06-30 10:39:50 +0530

    Commit
    Authentication

---
+++ API/Program.cs
@@ -0,0 +1,50 @@
+using JWTAuthenticationApp.Interfaces;
+using JWTAuthenticationApp.Services;
+using Microsoft.EntityFrameworkCore;
+using RoleBasedAuthorization.Data;
+using RoleBasedAuthorization.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IBaseRepo<string, User>, UserRepo>();
+builder.Services.AddScoped<ITokenGenerate, TokenService>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddDbContext<RoleBasedAuthorizationDbContext>(optionsAction: options => options.UseSqlServer(builder.Configuration.GetConnectionString(name: "AdminSkill")));
+
+builder.Services.AddCors(opts =>
+{
+    opts.AddPolicy("AngularCORS", options =>
+    {
+        options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseRouting();
+app.UseCors(options =>
+            options.WithOrigins("*").
+            AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+app.Run();
